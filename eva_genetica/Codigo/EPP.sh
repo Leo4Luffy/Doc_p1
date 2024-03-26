d4fb1ba547d2fffd3559da1d6bb7984ba518e1fd
leo <jollopezma@unal.edu.co>
2024-03-26 11:18:12 +0100

    Se obtuvieron los datos para el análisis tags+singletones.

--- eva_genetica/Codigo/EPP.sh
+++ eva_genetica/Codigo/EPP.sh
@@ -2,13 +2,13 @@
 
 ####
 # Titulo: Bash para calcular mediante blupf90 los valores genéticos en datos parciales y completos para el caracter edad al primer parto.
-# Autor(es): Jorge Leonardo López Martínez; Cristina Meneses González.
+# Autor(es): Jorge Leonardo López Martínez; Cristina Meneses González; Marisol Londoño Gil.
 # Requerimientos: renumf90, blupf90+.
 ####
 
 # Conjunto de datos completo
 
-cd ../Datos/eva_genomica/Beagle_50K/Edad_Primer_Parto/Completo
+cd ../../eva_genetica/Datos/eva_genomica/Beagle_50K/Edad_Primer_Parto/Completo
 
 echo "DATAFILE
 EPP.txt
@@ -48,17 +48,30 @@ INBREEDING
 pedigree
 (CO)VARIANCES
 1.0
-OPTION method VCE
-OPTION sol se
-OPTION callrate 0.00
-OPTION callrateAnim 0.00
-OPTION minfreq 0.00
-OPTION verify_parentage 3
-OPTION store_accuracy 5
-OPTION acctype 1.0
 OPTION remove_all_missing" > renum.par
 
 echo renum.par | ../../../../../BLUPF90/renumf90 | tee renum.log
+
+sed -i '/OPTION/d' renf90.par # Se insertan nuevas opciones en el renf90.par.
+ 
+echo "OPTION SNP_file ../genoEPP_2.txt
+OPTION saveCleanSNPs
+OPTION createGInverse 0
+OPTION createA22Inverse 0
+OPTION createGimA22i 0" >> renf90.par
+
+echo renf90.par | ../../../../../BLUPF90/preGSf90 | tee pregs.log # Hay dos individuos con genotipos duplicados. Se elimina uno de ellos, el que esta en la fila o linea 1960 mediante la opción "excludeSample" a continuación.
+
+sed -i '/OPTION/d' renf90.par # Se insertan nuevas opciones en el renf90.par.
+
+echo "OPTION SNP_file ../genoEPP_2.txt_clean
+OPTION excludeSample 1960
+OPTION method VCE
+OPTION sol se
+OPTION no_quality_control
+OPTION verify_parentage 3
+OPTION store_accuracy 5
+OPTION acctype 1.0" >> renf90.par
 
 echo renf90.par | ../../../../../BLUPF90/blupf90+ | tee blup.log
 
@@ -66,7 +79,7 @@ cd -
 
 # Conjunto de datos parcial
 
-cd ../Datos/eva_genomica/Beagle_50K/Edad_Primer_Parto/Parcial
+cd ../../eva_genetica/Datos/eva_genomica/Beagle_50K/Edad_Primer_Parto/Parcial
 
 echo "DATAFILE
 EPP.txt
@@ -106,17 +119,30 @@ INBREEDING
 pedigree
 (CO)VARIANCES
 1.0
-OPTION method VCE
-OPTION sol se
-OPTION callrate 0.00
-OPTION callrateAnim 0.00
-OPTION minfreq 0.00
-OPTION verify_parentage 3
-OPTION store_accuracy 5
-OPTION acctype 1.0
 OPTION remove_all_missing" > renum.par
 
 echo renum.par | ../../../../../BLUPF90/renumf90 | tee renum.log
+
+sed -i '/OPTION/d' renf90.par # Se insertan nuevas opciones en el renf90.par.
+ 
+echo "OPTION SNP_file ../genoEPP_2.txt
+OPTION saveCleanSNPs
+OPTION createGInverse 0
+OPTION createA22Inverse 0
+OPTION createGimA22i 0" >> renf90.par
+
+echo renf90.par | ../../../../../BLUPF90/preGSf90 | tee pregs.log # Hay dos individuos con genotipos duplicados. Se elimina uno de ellos, el que esta en la fila o linea 1960 mediante la opción "excludeSample" a continuación.
+
+sed -i '/OPTION/d' renf90.par # Se insertan nuevas opciones en el renf90.par.
+
+echo "OPTION SNP_file ../genoEPP_2.txt_clean
+OPTION excludeSample 1960
+OPTION method VCE
+OPTION sol se
+OPTION no_quality_control
+OPTION verify_parentage 3
+OPTION store_accuracy 5
+OPTION acctype 1.0" >> renf90.par
 
 echo renf90.par | ../../../../../BLUPF90/blupf90+ | tee blup.log
 
