16a2ea46b626ed95a0acbb2886ed6b7ba5d767dd
leo <jollopezma@unal.edu.co>
2024-04-30 17:08:53 +0200

    Se actualizaron algunas cositas.

--- eva_genetica/Codigo/EPP_IEP_ped.sh
+++ eva_genetica/Codigo/EPP_IEP_ped.sh
@@ -8,7 +8,9 @@ ulimit -s unlimited
 # Requerimientos: renumf90, gibbsf90+.
 ####
 
-cd ../../eva_genetica/Datos/eva_tradicional/EPP_IEP/
+# Conjunto de datos completo
+
+cd ../../eva_genetica/Datos/eva_tradicional/EPP_IEP/Completo/
 
 echo "DATAFILE
 EPP_IEP.txt
@@ -66,7 +68,7 @@ EFFECT
 RANDOM
 animal
 FILE
-../Pedigri.txt
+../../Pedigri.txt
 FILE_POS
 1 2 3
 PED_DEPTH
@@ -76,7 +78,7 @@ PED_DEPTH
 0.1 1.0
 OPTION remove_all_missing" > renum.par
 
-echo renum.par | ../../../BLUPF90/renumf90 | tee renum.log
+echo renum.par | ../../../../BLUPF90/renumf90 | tee renum.log
 
 sed -i '/OPTION/d' renf90.par # Se insertan nuevas opciones en el renf90.par.
 
@@ -85,7 +87,94 @@ OPTION save_halfway_samples 10000
 OPTION no_quality_control
 OPTION verify_parentage 3" >> renf90.par
 
-../../../BLUPF90/gibbsf90+ <<AA > gibbs.log
+../../../../BLUPF90/gibbsf90+ <<AA > gibbs.log
+renf90.par
+200000 50000
+50
+AA
+
+cd -
+
+# Conjunto de datos parcial
+
+cd ../../eva_genetica/Datos/eva_tradicional/EPP_IEP/Parcial/
+
+echo "DATAFILE
+EPP_IEP.txt
+TRAITS
+2 7
+FIELDS_PASSED TO OUTPUT
+
+WEIGHT(S)
+
+RESIDUAL_VARIANCE
+1.0 0.1
+0.1 1.0
+EFFECT
+3 0 cross alpha # Grupo de comparación para la edad al primer parto. Se trata como aleatorio.
+RANDOM
+diagonal
+(CO)VARIANCES
+1.0 0.0
+0.0 0.0
+EFFECT
+4 0 cross alpha # Identificación del toro (como efecto ambiental permanente del toro o la componente del toro que es común a todas las hembras que se cubren con el mismo reproductor).
+RANDOM
+diagonal
+(CO)VARIANCES
+1.0 0.0
+0.0 0.0
+EFFECT
+5 0 cross alpha # Sexo de la cría en el primer parto.
+EFFECT
+6 0 cross alpha # Edad del toro en el primer parto (< 3 años, >= 3 años, < 4 años >= 4 años, < 5 años).
+EFFECT
+0 8 cross alpha # Edad al primer parto como efecto para el IEP (edad al primer parto de la hembra clasificada en tres grupos: < 2.5 años, >= 2.5 años, <= 3 años, > 3 años).
+EFFECT
+0 9 cross alpha # Grupo de comparación para el intervalo entre los partos uno y dos.
+RANDOM
+diagonal
+(CO)VARIANCES
+0.0 0.0
+0.0 1.0
+EFFECT
+0 10 cross alpha # Identificación del toro (como efecto ambiental permanente del toro o la componente del toro que es común a todas las hembras que se cubren con el mismo reproductor).
+RANDOM
+diagonal
+(CO)VARIANCES
+0.0 0.0
+0.0 1.0
+EFFECT
+0 11 cross alpha # Sexo de la cría en el segundo parto.
+EFFECT
+0 12 cross alpha # Edad del toro en el segundo parto (< 3 años, >= 3 años, < 4 años >= 4 años, < 5 años).
+EFFECT
+0 13 cross alpha # Raza del toro (Avileña Negra-Ibérica, Charoles, Limousín).
+EFFECT
+1 1 cross alpha # Posición del individuo.
+RANDOM
+animal
+FILE
+../../Pedigri.txt
+FILE_POS
+1 2 3
+PED_DEPTH
+0
+(CO)VARIANCES
+1.0 0.1
+0.1 1.0
+OPTION remove_all_missing" > renum.par
+
+echo renum.par | ../../../../BLUPF90/renumf90 | tee renum.log
+
+sed -i '/OPTION/d' renf90.par # Se insertan nuevas opciones en el renf90.par.
+
+echo "OPTION solution mean
+OPTION save_halfway_samples 10000
+OPTION no_quality_control
+OPTION verify_parentage 3" >> renf90.par
+
+../../../../BLUPF90/gibbsf90+ <<AA > gibbs.log
 renf90.par
 200000 50000
 50
--- eva_genetica/Codigo/pDtte_ped.sh
+++ eva_genetica/Codigo/pDtte_ped.sh
@@ -8,7 +8,9 @@ ulimit -s unlimited
 # Requerimientos: renumf90, gibbsf90+.
 ####
 
-cd ../../eva_genetica/Datos/eva_tradicional/peso_Dtte/
+# Conjunto de datos completo
+
+cd ../../eva_genetica/Datos/eva_tradicional/peso_Dtte/Completo/
 
 echo "DATAFILE
 pDtte.txt
@@ -35,7 +37,7 @@ animal
 OPTIONAL
 mat mpe
 FILE
-../Pedigri.txt
+../../Pedigri.txt
 FILE_POS
 1 2 3
 PED_DEPTH
@@ -47,7 +49,7 @@ PED_DEPTH
 1.0
 OPTION remove_all_missing" > renum.par
 
-echo renum.par | ../../../BLUPF90/renumf90 | tee renum.log
+echo renum.par | ../../../../BLUPF90/renumf90 | tee renum.log
 
 sed -i '/OPTION/d' renf90.par # Se insertan nuevas opciones en el renf90.par.
 
@@ -56,7 +58,65 @@ OPTION save_halfway_samples 10000
 OPTION no_quality_control
 OPTION verify_parentage 3" >> renf90.par
 
-../../../BLUPF90/gibbsf90+ <<AA > gibbs.log
+../../../../BLUPF90/gibbsf90+ <<AA > gibbs.log
+renf90.par
+200000 50000
+50
+AA
+
+cd -
+
+# Conjunto de datos parcial
+
+cd ../../eva_genetica/Datos/eva_tradicional/peso_Dtte/Parcial/
+
+echo "DATAFILE
+pDtte.txt
+TRAITS
+2
+FIELDS_PASSED TO OUTPUT
+
+WEIGHT(S)
+
+RESIDUAL_VARIANCE
+1.0
+EFFECT
+3 cov # Edad al destete como covariable.
+EFFECT
+4 cross alpha # Sexo del individuo como efecto fijo.
+EFFECT
+5 cross alpha # Edad de la madre (clasificado en seis grupos) como efecto fijo.
+EFFECT
+6 cross alpha # Grupo de comparación como efecto fijo.
+EFFECT
+1 cross alpha # Posición del individuo.
+RANDOM
+animal
+OPTIONAL
+mat mpe
+FILE
+../../Pedigri.txt
+FILE_POS
+1 2 3
+PED_DEPTH
+0
+(CO)VARIANCES
+1.0   0.1    
+0.1   1.0
+(CO)VARIANCES_MPE
+1.0
+OPTION remove_all_missing" > renum.par
+
+echo renum.par | ../../../../BLUPF90/renumf90 | tee renum.log
+
+sed -i '/OPTION/d' renf90.par # Se insertan nuevas opciones en el renf90.par.
+
+echo "OPTION solution mean
+OPTION save_halfway_samples 10000
+OPTION no_quality_control
+OPTION verify_parentage 3" >> renf90.par
+
+../../../../BLUPF90/gibbsf90+ <<AA > gibbs.log
 renf90.par
 200000 50000
 50
