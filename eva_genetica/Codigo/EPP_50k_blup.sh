358fd1a3ba894e81e63f9e83bf5e2aca19d29aac
leo <jollopezma@unal.edu.co>
2024-05-09 18:42:27 +0200

    Se inicio con unas pequeñas correcciones.

---
+++ eva_genetica/Codigo/EPP_50k_blup.sh
@@ -0,0 +1,80 @@
+ulimit -s unlimited
+
+#!/bin/bash
+
+####
+# Titulo: Bash para calcular mediante blupf90 los valores genéticos en datos parciales y completos para el caracter edad al primer parto.
+# Autor(es): Jorge Leonardo López Martínez; Cristina Meneses González; Marisol Londoño Gil.
+# Requerimientos: renumf90, blupf90+.
+####
+
+# Conjunto de datos completo
+
+cd ../../eva_genetica/Datos/eva_genomica/Beagle_50K/Edad_Primer_Parto_blup/Completo
+
+echo "DATAFILE
+EPP.txt
+TRAITS
+2
+FIELDS_PASSED TO OUTPUT
+
+WEIGHT(S)
+
+RESIDUAL_VARIANCE
+1.0
+EFFECT
+3 cross alpha # Grupo de comparación para la edad al primer parto. Se trata como aleatorio para los dos caracteres de fertilidad.
+RANDOM
+diagonal
+(CO)VARIANCES
+1.0
+EFFECT
+4 cross alpha # Identificación del toro (como efecto ambiental permanente del toro o la componente del toro que es común a todas las hembras que se cubren con el mismo reproductor).
+RANDOM
+diagonal
+(CO)VARIANCES
+1.0
+EFFECT
+5 cross alpha # Sexo de la cría en el primer parto.
+EFFECT
+6 cross alpha # Edad del toro en el primer parto (< 3 años, >= 3 años, < 4 años >= 4 años, < 5 años).
+EFFECT
+1 cross alpha # Posición del individuo.
+RANDOM
+animal
+FILE
+../../../Pedigri.txt
+FILE_POS
+1 2 3
+SNP_FILE
+../../Genotipos_2.txt
+PED_DEPTH
+0 # Profundidad de búsqueda de pedigrí (predeterminado igual a 3); todos los pedigríes se cargan si p = 0.
+(CO)VARIANCES
+1.0
+OPTION remove_all_missing" > renum.par
+
+echo renum.par | ../../../../../BLUPF90/renumf90 | tee renum.log
+
+sed -i '/OPTION/d' renf90.par # Se insertan nuevas opciones en el renf90.par.
+ 
+echo "OPTION SNP_file ../../Genotipos_2.txt
+OPTION excludeSample 2383
+OPTION saveCleanSNPs
+OPTION createGInverse 0
+OPTION createA22Inverse 0
+OPTION createGimA22i 0" >> renf90.par
+
+echo renf90.par | ../../../../../BLUPF90/preGSf90 | tee pregs.log # Hay dos individuos con genotipos duplicados. Se elimina uno de ellos, el que esta en la fila o linea 2383 mediante la opción "excludeSample".
+
+sed -i '/OPTION/d' renf90.par # Se insertan nuevas opciones en el renf90.par.
+
+echo "OPTION SNP_file ../../Genotipos_2.txt_clean
+OPTION no_quality_control
+OPTION verify_parentage 3
+OPTION store_accuracy 5
+OPTION acctype 1.0" >> renf90.par
+
+echo renf90.par | ../../../../../BLUPF90/blupf90+ | tee blup.log
+
+cd -
